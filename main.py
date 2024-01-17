41a32e3fd03fd4513011608fe15bb134aafffe6b
aoroudjev <a.oroudjev@gmail.com>
2024-01-16 23:06:32 -0800

    Remove grid system, add display logic.

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-from typing import Tuple, List
-
 import pygame
 from pygame.locals import *
 from snake import Snake
 
-WINDOW_SIZE = (300, 300)
+WINDOW_SIZE = (500, 500)
 GRID_SQUARE_SIZE = 20
 
 
-def draw_grid(snake_list: List[Tuple]):
-    for x in range(0, WINDOW_SIZE[0], GRID_SQUARE_SIZE):
-        for y in range(0, WINDOW_SIZE[1], GRID_SQUARE_SIZE):
-            rect = pygame.Rect(x, y, GRID_SQUARE_SIZE, GRID_SQUARE_SIZE)
-            if (y, x) in snake_list:
-                pygame.draw.rect(screen, "red", rect, 2)
-            else:
-                pygame.draw.rect(screen, "white", rect, 1)
+def draw_grid() -> None:
+    for x in range(0, WINDOW_SIZE[0] // GRID_SQUARE_SIZE):
+        for y in range(0, WINDOW_SIZE[1] // GRID_SQUARE_SIZE):
+            rect = pygame.Rect(x * GRID_SQUARE_SIZE,
+                               y * GRID_SQUARE_SIZE,
+                               GRID_SQUARE_SIZE,
+                               GRID_SQUARE_SIZE)
+            pygame.draw.rect(screen, "white", rect, 1)
+
+
+def draw_snake(snake: Snake):
+    for snake_x, snake_y in snake.snake_list:
+        snake_seg = pygame.Rect(snake_x * GRID_SQUARE_SIZE,
+                                snake_y * GRID_SQUARE_SIZE,
+                                GRID_SQUARE_SIZE,
+                                GRID_SQUARE_SIZE)
+        pygame.draw.rect(screen, "red", snake_seg)
 
 
 def main():
@@ -32,30 +39,34 @@ def main():
     clock = pygame.time.Clock()
     pygame.display.update()
 
-    snake = Snake((0, 0))
+    snake = Snake(tuple(int((elem // GRID_SQUARE_SIZE) / 2) for elem in WINDOW_SIZE))
+    last_time = pygame.time.get_ticks()
 
     # Event loop
     while True:
-
-        clock.tick(1)
-
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
-            # elif event.type == KEYDOWN:
-            #     if event.key == K_UP:
-            #         snake.direction = 'up'
-            #     if event.key == K_DOWN:
-            #         snake.direction = 'down'
-            #     if event.key == K_LEFT:
-            #         snake.direction = 'left'
-            #     if event.key == K_RIGHT:
-            #         snake.direction = 'right'
+            elif event.type == KEYDOWN:
+                if event.key == K_UP and snake.direction != 'down':
+                    snake.set_direction('up')
+                if event.key == K_DOWN and snake.direction != 'up':
+                    snake.set_direction('down')
+                if event.key == K_LEFT and snake.direction != 'right':
+                    snake.set_direction('left')
+                if event.key == K_RIGHT and snake.direction != 'left':
+                    snake.set_direction('right')
 
-        print(snake.snake_list)
-        draw_grid(snake.snake_list)
+        print(f'head: {snake.snake_list}, direction: {snake.get_direction()}')
+        # draw_grid()
+        draw_snake(snake)
         pygame.display.update()
-        snake.update()
+        current_time = pygame.time.get_ticks()
+        if current_time - last_time > 100:
+            snake.update()
+            last_time = current_time
+        screen.fill('black')
 
 
 if __name__ == '__main__':
